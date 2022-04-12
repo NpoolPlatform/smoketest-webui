ebbc69ff59849f4978a608b5f72ee26432004ba0
Zhao KK <kikakkz@hotmail.com>
2022-04-12 23:21:24 +0800

    Add default copyright

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -2,7 +2,7 @@
   <q-footer>
     <div class='row announcement main'>
       <q-icon class='icon start' name='copyright' />
-      {{ $t('MSG_DASHBOARD_COPYRIGHT') }}
+      {{ $t('MSG_DASHBOARD_COPYRIGHT', { THIS_YEAR: new Date().getFullYear() }) }}
     </div>
   </q-footer>
 </template>
--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -5,5 +5,6 @@ export default {
   MSG_GET_LANGS: 'Get languages',
   MSG_GET_LANGS_FAIL: 'You failed to get languages',
   MSG_GET_LANG_MESSAGES: 'Get lang messages',
-  MSG_GET_LANG_MESSAGES_FAIL: 'Get lang messages fails'
+  MSG_GET_LANG_MESSAGES_FAIL: 'Get lang messages fails',
+  MSG_DASHBOARD_COPYRIGHT: 'Copyright reserved by 2021-{THIS_YEAR} Entropy Pool Team'
 }
