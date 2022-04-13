d9ac0a18679094f378c41ce6e2c91026a3351607
Zhao KK <kikakkz@hotmail.com>
2022-04-13 18:53:25 +0800

    Better lang loader

--- src/components/lang/LangLoader.vue
+++ src/components/lang/LangLoader.vue
@@ -1,12 +1,27 @@
 <script setup lang='ts'>
-import { onMounted } from 'vue'
-import { NotificationType, useLangStore } from 'npool-cli-v2'
+import { onMounted, computed, watch } from 'vue'
+import { NotificationType, useLangStore, useLocaleStore } from 'npool-cli-v2'
 import { useI18n } from 'vue-i18n'
 
 // eslint-disable-next-line @typescript-eslint/unbound-method
 const { t } = useI18n({ useScope: 'global' })
 
 const lang = useLangStore()
+const locale = useLocaleStore()
+const langID = computed(() => locale.CurLang?.ID)
+watch(langID, () => {
+  lang.getLangMessages({
+    LangID: langID.value as string,
+    Message: {
+      Error: {
+        Title: t('MSG_GET_LANG_MESSAGES'),
+        Message: t('MSG_GET_LANG_MESSAGES_FAIL'),
+        Popup: true,
+        Type: NotificationType.Error
+      }
+    }
+  })
+})
 
 onMounted(() => {
   lang.getLangs({
