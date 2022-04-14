3ade05c77bb11a7a8db86751f882871f6e809709
Zhao KK <kikakkz@hotmail.com>
2022-04-14 19:12:34 +0800

    Add good list

--- src/components/good/Good.vue
+++ src/components/good/Good.vue
@@ -6,18 +6,35 @@
     :rows='coins'
     row-key='ID'
     :loading='coinLoading'
-    :rows-per-page-options='[20]'
+    :rows-per-page-options='[10]'
   />
+  <q-table
+    dense
+    flat
+    :title='$t("MSG_GOODS")'
+    :rows='goods'
+    row-key='ID'
+    :loading='goodLoading'
+    :rows-per-page-options='[10]'
+  />
+  <q-card>
+    <q-card-section class='bg-primary text-white'>
+      {{ $t('MSG_ADVERTISEMENT_POSITION') }}
+    </q-card-section>
+  </q-card>
 </template>
 
 <script setup lang='ts'>
-import { NotificationType, useCoinStore } from 'npool-cli-v2'
+import { buildGoods, NotificationType, useCoinStore, useGoodStore } from 'npool-cli-v2'
 import { computed, onMounted, ref } from 'vue'
 
 const coin = useCoinStore()
-
 const coins = computed(() => coin.Coins)
 const coinLoading = ref(true)
+
+const good = useGoodStore()
+const goods = computed(() => buildGoods(good.Goods))
+const goodLoading = ref(true)
 
 onMounted(() => {
   coin.getCoins({
@@ -31,6 +48,19 @@ onMounted(() => {
     }
   }, () => {
     coinLoading.value = false
+  })
+
+  good.getGoods({
+    Message: {
+      Error: {
+        Title: 'MSG_GET_GOODS',
+        Message: 'MSG_GET_GOODS_FAIL',
+        Popup: true,
+        Type: NotificationType.Error
+      }
+    }
+  }, () => {
+    goodLoading.value = false
   })
 })
 
