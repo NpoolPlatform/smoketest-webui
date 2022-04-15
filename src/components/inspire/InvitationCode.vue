690f40e2aefa91a8beb0ecb64ad9b597a2cf471d
Zhao KK <kikakkz@hotmail.com>
2022-04-15 22:27:52 +0800

    Add invitation code

--- src/components/inspire/InvitationCode.vue
+++ src/components/inspire/InvitationCode.vue
@@ -3,7 +3,7 @@
     dense
     flat
     :title='$t("MSG_INVITATION_CODES")'
-    :rows='codes'
+    :rows='ecodes'
     row-key='ID'
     :loading='codeLoading'
     :rows-per-page-options='[10]'
@@ -11,7 +11,7 @@
 </template>
 
 <script setup lang='ts'>
-import { useAdminInspireStore, NotificationType } from 'npool-cli-v2'
+import { useAdminInspireStore, NotificationType, InvitationCode, useUsersStore } from 'npool-cli-v2'
 import { computed, onMounted, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
@@ -21,6 +21,19 @@ const { t } = useI18n({ useScope: 'global' })
 const inspire = useAdminInspireStore()
 const codes = computed(() => inspire.InvitationCodes)
 const codeLoading = ref(true)
+
+interface Code extends InvitationCode {
+  EmailAddress: string
+  PhoneNO: string
+}
+
+const user = useUsersStore()
+const ecodes = computed(() => Array.from(codes.value).map((code) => {
+  const myCode = code as unknown as Code
+  myCode.EmailAddress = user.getUserByID(code.UserID)?.User.EmailAddress as string
+  myCode.PhoneNO = user.getUserByID(code.UserID)?.User.PhoneNO as string
+  return myCode
+}))
 
 onMounted(() => {
   inspire.getInvitationCodes({
@@ -34,6 +47,19 @@ onMounted(() => {
     }
   }, () => {
     codeLoading.value = false
+  })
+
+  user.getUsers({
+    Message: {
+      Error: {
+        Title: t('MSG_GET_USERS'),
+        Message: t('MSG_GET_USERS_FAIL'),
+        Popup: true,
+        Type: NotificationType.Error
+      }
+    }
+  }, () => {
+    // TODO
   })
 })
 
