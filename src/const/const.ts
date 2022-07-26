05579df43a1b58eb9c6dd1463cf8818b9b716fab
hong-t <th13564039482@163.com>
2022-07-26 03:57:56 +0000

    update domain for dashboard

--- src/const/const.ts
+++ src/const/const.ts
@@ -8,6 +8,13 @@ if (window.location.host.includes('.procyon.vip')) {
   AppID = 'ff2c5d50-be56-413e-aba5-9c7ad888a769'
 }
 
+if (window.location.host.includes('.procyon.npool.top')) 
+  AppID = 'ff2c5d50-be56-413e-aba5-9c7ad888a769'
+}
+if (window.location.host.includes('.ohhash.npool.top')) 
+  AppID = 'ab7895dd-c4d1-48bd-be77-c0f255556a36'
+}
+
 export {
   AppID
 }
