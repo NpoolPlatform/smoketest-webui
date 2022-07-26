2adffd47a5278a75298fd341c731348f0763b724
hong-t <th13564039482@163.com>
2022-07-26 04:35:38 +0000

    fix const.ts

--- src/const/const.ts
+++ src/const/const.ts
@@ -7,11 +7,10 @@ if (window.location.host.includes('.ohhash.com')) {
 if (window.location.host.includes('.procyon.vip')) {
   AppID = 'ff2c5d50-be56-413e-aba5-9c7ad888a769'
 }
-
-if (window.location.host.includes('.procyon.npool.top')) 
+if (window.location.host.includes('.procyon.npool.top')) {
   AppID = 'ff2c5d50-be56-413e-aba5-9c7ad888a769'
 }
-if (window.location.host.includes('.ohhash.npool.top')) 
+if (window.location.host.includes('.ohhash.npool.top')) {
   AppID = 'ab7895dd-c4d1-48bd-be77-c0f255556a36'
 }
 
