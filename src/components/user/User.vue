e8b2ed0c7ffa16e9dfd28f4b52cdd9438389480d
lidaqiang <dqli161@163.com>
2022-08-29 21:41:42 +0800

    User update

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -16,26 +16,31 @@
 </template>
 
 <script setup lang='ts'>
-import { NotificationType, useUsersStore } from 'npool-cli-v2'
+import { NotifyType, useAdminUserStore } from 'npool-cli-v4'
 import { computed, onMounted, ref } from 'vue'
 
-const user = useUsersStore()
-const users = computed(() => Array.from(user.Users).map((user) => user.User))
-const userLoading = ref(true)
+const user = useAdminUserStore()
+const users = computed(() => user.Users.Users)
+const userLoading = ref(false)
 
 onMounted(() => {
-  user.getUsers({
-    Message: {
-      Error: {
-        Title: 'MSG_GET_USERS',
-        Message: 'MSG_GET_USERS_FAIL',
-        Popup: true,
-        Type: NotificationType.Error
+  if (user.Users.Users.length === 0) {
+    userLoading.value = true
+    user.getUsers({
+      Offset: 0,
+      Limit: 100,
+      Message: {
+        Error: {
+          Title: 'MSG_GET_USERS',
+          Message: 'MSG_GET_USERS_FAIL',
+          Popup: true,
+          Type: NotifyType.Error
+        }
       }
-    }
-  }, () => {
-    userLoading.value = false
-  })
+    }, () => {
+      userLoading.value = false
+    })
+  }
 })
 
 </script>
