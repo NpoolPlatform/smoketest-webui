41162fbe93bcaed55664aaf8e571320ab160c7d4
lidaqiang <dqli161@163.com>
2022-08-31 14:15:25 +0800

    Add User Column

--- src/components/inspire/SpecialOffer.vue
+++ src/components/inspire/SpecialOffer.vue
@@ -8,6 +8,7 @@
     :rows-per-page-options='[10]'
     selection='single'
     v-model:selected='selectedUser'
+    :columns='columns'
   >
     <template #top-right>
       <div class='row indent flat'>
@@ -100,7 +101,43 @@ import { useI18n } from 'vue-i18n'
 
 // eslint-disable-next-line @typescript-eslint/unbound-method
 const { t } = useI18n({ useScope: 'global' })
-
+const columns = computed(() => [
+  {
+    name: 'AppID',
+    label: t('MSG_APP_ID'),
+    field: (row: User) => row.AppID
+  },
+  {
+    name: 'UserID',
+    label: t('MSG_USER_ID'),
+    field: (row: User) => row.ID
+  },
+  {
+    name: 'EmailAddress',
+    label: t('MSG_EMAIL_ADDRESS'),
+    field: (row: User) => row.EmailAddress
+  },
+  {
+    name: 'PhoneNO',
+    label: t('MSG_PHONE_NO'),
+    field: (row: User) => row.PhoneNO
+  },
+  {
+    name: 'Roles',
+    label: t('MSG_ROLES'),
+    field: (row: User) => row.Roles?.join(',')
+  },
+  {
+    name: 'IDNUMBER',
+    label: t('MSG_IDNUMBER'),
+    field: (row: User) => row.IDNumber
+  },
+  {
+    name: 'CreatedAt',
+    label: t('MSG_CREATEDAT'),
+    field: (row: User) => formatTime(row.CreatedAt)
+  }
+])
 const user = useAdminUserStore()
 const users = computed(() => user.Users.Users)
 const username = ref('')
