b2cdca19eaa0791e0c35d8bf9890bde5edaba8f2
lidaqiang <dqli161@163.com>
2022-09-19 16:26:37 +0800

    Change Logo In Header

--- src/components/logo/Logo.vue
+++ src/components/logo/Logo.vue
@@ -1,11 +1,13 @@
 <template>
-  <q-img :src='logoImg' />
+  <q-img :src='logoImg' class='logo' />
 </template>
 
 <script setup lang='ts'>
 
-import logoImg from '../../assets/logo.png'
+import logoImg from '../../assets/logo-text.png'
 
 </script>
 <style lang='sass' scoped>
+  .logo
+    width: 200px
 </style>
