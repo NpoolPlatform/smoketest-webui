2a13af16dfa14b9dae72329eb6ecb3f1e7e33573
lidaqiang <dqli161@163.com>
2022-09-26 13:30:32 +0800

    Change AppID To Procyon

--- src/const/const.ts
+++ src/const/const.ts
@@ -1,5 +1,5 @@
 // Default is procyon app id
-let AppID = 'ef936d96-d8bb-40ee-a9b6-8c5af3d41bf8'
+let AppID = 'ff2c5d50-be56-413e-aba5-9c7ad888a769'
 
 if (window.location.host.includes('.ohhash.com')) {
   AppID = 'ab7895dd-c4d1-48bd-be77-c0f255556a36'
