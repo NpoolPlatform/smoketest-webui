0da698316e41857b7e565ba5345fa6e562ad4c56
Zhao KK <kikakkz@hotmail.com>
2022-09-27 02:15:08 +0000

    update appid

--- src/const/const.ts
+++ src/const/const.ts
@@ -7,11 +7,17 @@ if (window.location.host.includes('.ohhash.com')) {
 if (window.location.host.includes('.procyon.vip')) {
   AppID = 'ff2c5d50-be56-413e-aba5-9c7ad888a769'
 }
+if (window.location.host.includes('.lpsecretbase.com')) {
+  AppID = '9141b71a-ff6e-4229-8cf5-cc71459e9803'
+}
 if (window.location.host.includes('.procyon.npool.top')) {
   AppID = 'ff2c5d50-be56-413e-aba5-9c7ad888a769'
 }
 if (window.location.host.includes('.ohhash.npool.top')) {
   AppID = 'ab7895dd-c4d1-48bd-be77-c0f255556a36'
+}
+if (window.location.host.includes('.lpsecretbase.npool.top')) {
+  AppID = '9141b71a-ff6e-4229-8cf5-cc71459e9803'
 }
 
 export {
