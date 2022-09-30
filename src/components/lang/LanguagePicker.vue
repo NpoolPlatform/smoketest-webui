c6284794cfe43856e07a6e9e676a991df4106fe4
lidaqiang <dqli161@163.com>
2022-09-30 13:23:36 +0800

    Add Language Picker

---
+++ src/components/lang/LanguagePicker.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-select
+    v-model='lang'
+    :options='languages'
+    options-selected-class='text-deep-orange'
+    emit-value
+    map-options
+    @update:model-value='onUpdate'
+  >
+    <template #option='scope'>
+      <q-item v-bind='scope.itemProps'>
+        <q-item-section avatar>
+          <q-img fit='contain' class='icon' :src='scope.opt.icon' />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ scope.opt.label }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </template>
+  </q-select>
+</template>
+<script setup lang='ts'>
+import { useLocaleStore } from 'npool-cli-v2'
+import { computed, defineEmits, defineProps, toRef, ref, onMounted } from 'vue'
+
+interface Props {
+  language: string
+}
+const props = defineProps<Props>()
+const language = toRef(props, 'language')
+const lang = ref(language.value)
+
+const locale = useLocaleStore()
+const languages = computed(() => Array.from(locale.Languages).map((el) => {
+  return {
+    value: el.ID,
+    label: el.Name,
+    icon: el.Logo
+  }
+}))
+
+const emit = defineEmits<{(e: 'update:language', language: string): void}>()
+const onUpdate = () => {
+  emit('update:language', lang.value)
+}
+
+onMounted(() => {
+  if (!language.value) {
+    lang.value = languages.value.length > 0 ? languages.value[0].value : ''
+    emit('update:language', lang.value)
+  }
+})
+</script>
