e77d972b41dc6ce548a7e6b6df84d403deb66480
lidaqiang <dqli161@163.com>
2022-11-28 15:25:01 +0800

    Update Commission Page

--- src/components/inspire/Commission.vue
+++ src/components/inspire/Commission.vue
@@ -2,35 +2,6 @@
   <q-table
     dense
     flat
-    :title='$t("MSG_USERS")'
-    :rows='displayUsers'
-    row-key='ID'
-    :rows-per-page-options='[10]'
-    selection='single'
-    v-model:selected='selectedUser'
-    :columns='columns'
-  >
-    <template #top-right>
-      <div class='row indent flat'>
-        <q-input
-          dense
-          class='small'
-          v-model='username'
-          :label='$t("MSG_USERNAME")'
-        />
-        <q-btn
-          dense
-          flat
-          class='btn flat'
-          :label='$t("MSG_CREATE")'
-          @click='onCreatePurchaseAmountSettingClick'
-        />
-      </div>
-    </template>
-  </q-table>
-  <q-table
-    dense
-    flat
     :title='$t("MSG_COMMISSION_SETTING")'
     :rows='setting'
     row-key='ID'
@@ -59,7 +30,7 @@
           flat
           class='btn flat'
           :label='$t("MSG_CREATE")'
-          @click='onCreatePurchaseAmountSettingClick'
+          @click='onCreate'
         />
       </div>
     </template>
@@ -79,17 +50,17 @@
         <span>{{ $t('MSG_CREATE_PURCHASE_AMOUNT_SETTING') }}</span>
       </q-card-section>
       <q-card-section>
-        <span>{{ $t('MSG_USERNAME') }}: {{ selectedUser[0]?.EmailAddress?.length ? selectedUser[0]?.EmailAddress : selectedUser[0]?.PhoneNO }}</span>
+        <AppUserSelector v-model:id='target.UserID' />
       </q-card-section>
       <q-card-section>
-        <q-select dense :options='myGoods' v-model='selectedGood' :label='$t("MSG_GOOD")' />
-        <q-input type='number' v-model='target.Amount' :label='$t("MSG_AMOUNT")' />
-        <q-input type='number' v-model='target.Percent' :label='$t("MSG_PERCENT")' />
-        <q-input v-model='target.BadgeLarge' :label='$t("MSG_BADGE_LARGE")' />
-        <q-input v-model='target.BadgeSmall' :label='$t("MSG_BADGE_SMALL")' />
+        <AppGoodSelector v-model:id='target.GoodID' />
+        <q-input type='number' v-model.number='target.Amount' :label='$t("MSG_AMOUNT")' />
+        <q-input type='number' v-model.number='target.Percent' :label='$t("MSG_PERCENT")' suffix='%' />
+        <q-input type='number' v-model='target.BadgeLarge' :label='$t("MSG_BADGE_LARGE")' />
+        <q-input type='number' v-model='target.BadgeSmall' :label='$t("MSG_BADGE_SMALL")' />
         <q-input v-model='target.Title' :label='$t("MSG_TITLE")' />
-        <q-input type='date' v-model='start' :label='$t("MSG_START")' />
-        <q-input type='date' v-model='end' :label='$t("MSG_END")' />
+        <DatePicker v-model:date='target.Start' :label='$t("MSG_START")' />
+        <DatePicker v-model:date='target.End' :label='$t("MSG_END")' />
       </q-card-section>
       <q-item class='row'>
         <q-btn class='btn round alt' :label='$t("MSG_SUBMIT")' @click='onSubmit' />
@@ -102,58 +73,21 @@
 <script setup lang='ts'>
 import {
   NotificationType,
-  useCoinStore,
   CommissionCoinSetting,
   PurchaseAmountSetting,
-  InvalidID,
   useCommissionStore,
   usePurchaseAmountSettingStore
 } from 'npool-cli-v2'
-import { AppGood, formatTime, NotifyType, useAdminAppGoodStore, useAdminUserStore, User } from 'npool-cli-v4'
-import { computed, onMounted, ref, watch } from 'vue'
+import { useAdminAppCoinStore, useAdminAppGoodStore, useAdminUserStore } from 'npool-cli-v4'
+import { getCoins } from 'src/api/coin'
+import { computed, defineAsyncComponent, onMounted, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
-
 // eslint-disable-next-line @typescript-eslint/unbound-method
 const { t } = useI18n({ useScope: 'global' })
-const columns = computed(() => [
-  {
-    name: 'AppID',
-    label: t('MSG_APP_ID'),
-    field: (row: User) => row.AppID
-  },
-  {
-    name: 'UserID',
-    label: t('MSG_USER_ID'),
-    field: (row: User) => row.ID
-  },
-  {
-    name: 'EmailAddress',
-    label: t('MSG_EMAIL_ADDRESS'),
-    field: (row: User) => row.EmailAddress
-  },
-  {
-    name: 'PhoneNO',
-    label: t('MSG_PHONE_NO'),
-    field: (row: User) => row.PhoneNO
-  },
-  {
-    name: 'Roles',
-    label: t('MSG_ROLES'),
-    field: (row: User) => row.Roles?.join(',')
-  },
-  {
-    name: 'IDNUMBER',
-    label: t('MSG_IDNUMBER'),
-    field: (row: User) => row.IDNumber
-  },
-  {
-    name: 'CreatedAt',
-    label: t('MSG_CREATEDAT'),
-    field: (row: User) => formatTime(row.CreatedAt)
-  }
-])
-const coin = useCoinStore()
-const user = useAdminUserStore()
+
+const AppGoodSelector = defineAsyncComponent(() => import('src/components/good/AppGoodSelector.vue'))
+const AppUserSelector = defineAsyncComponent(() => import('src/components/user/AppUserSelector.vue'))
+const DatePicker = defineAsyncComponent(() => import('src/components/date/DatePicker.vue'))
 
 interface CoinSetting extends CommissionCoinSetting {
   CoinName: string
@@ -165,85 +99,69 @@ interface AmountSetting extends PurchaseAmountSetting {
   GoodName: string
 }
 
+interface MyPurchaseAmountSetting extends PurchaseAmountSetting {
+  UserID: string;
+}
+
+const coin = useAdminAppCoinStore()
+const user = useAdminUserStore()
+const good = useAdminAppGoodStore()
+
 const commission = useCommissionStore()
 const setting = computed(() => commission.CommissionSetting ? [commission.CommissionSetting] : [])
 const coinSettings = computed(() => Array.from(commission.CommissionCoinSettings).map((el) => {
-  const s = el as unknown as CoinSetting
+  const s = el as CoinSetting
   s.CoinName = coin.getCoinByID(s.CoinTypeID)?.Name as string
   return s
 }))
 
-const good = useAdminAppGoodStore()
-const goods = computed(() => good.AppGoods.AppGoods)
-
-interface MyGood {
-  label: string
-  value: AppGood
-}
-const myGoods = computed(() => Array.from(goods.value).map((el) => {
-  return {
-    label: el.GoodName + '(' + (el.GoodID) + ')',
-    value: el
-  } as MyGood
-}))
-const selectedGood = computed({
-  get: () => {
-    const g = good.getGoodByID(target.value.GoodID)
-    if (g) {
-      return {
-        label: g.GoodName + '(' + (g.GoodID) + ')',
-        value: g
-      } as MyGood
-    }
-    return undefined as unknown as MyGood
-  },
-  set: (val: MyGood) => {
-    target.value.GoodID = val.value.GoodID
-  }
-})
-
 const purchaseAmount = usePurchaseAmountSettingStore()
 const purchaseAmountSettings = computed(() => purchaseAmount.PurchaseAmountSettings)
-
-const users = computed(() => user.Users.Users)
-const selectedUser = ref([] as Array<User>)
-const username = ref('')
-const displayUsers = computed(() => users.value.filter((user) => {
-  return user.EmailAddress?.toLowerCase().includes(username.value.toLowerCase()) ||
-        user.PhoneNO?.toLowerCase().includes(username.value.toLowerCase())
-}))
-
 const amountSettings = computed(() => Array.from(purchaseAmountSettings.value).map((el) => {
-  const s = el as unknown as AmountSetting
+  const s = el as AmountSetting
   s.EmailAddress = user.getUserByID(s.UserID as string)?.EmailAddress
   s.PhoneNO = user.getUserByID(s.UserID as string)?.PhoneNO
-  s.GoodName = ''
-  const index = goods.value.findIndex((gel) => gel.GoodID === el.GoodID)
-  if (index >= 0) {
-    s.GoodName = goods.value[index].GoodName
-  }
+  s.GoodName = good.getGoodByID(el.GoodID)?.GoodName as string
   return s
 }))
 
-const getUsers = (offset: number, limit: number) => {
-  user.getUsers({
-    Offset: offset,
-    Limit: limit,
+const showing = ref(false)
+const target = ref({} as MyPurchaseAmountSetting)
+
+const onCreate = () => {
+  showing.value = true
+}
+
+const onMenuHide = () => {
+  showing.value = false
+  target.value = {} as MyPurchaseAmountSetting
+}
+
+const onCancel = () => {
+  onMenuHide()
+}
+
+const onSubmit = () => {
+  if (target.value.Start <= 0) {
+    return
+  }
+  purchaseAmount.createUserPurchaseAmountSetting({
+    TargetUserID: target.value.UserID,
+    Info: target.value,
     Message: {
       Error: {
-        Title: 'MSG_GET_USERS',
-        Message: 'MSG_GET_USERS_FAIL',
+        Title: t('MSG_CREATE_USER_PURCHASE_AMOUNT_SETTING'),
+        Message: t('MSG_CREATE_USER_PURCHASE_AMOUNT_SETTING_FAIL'),
         Popup: true,
-        Type: NotifyType.Error
+        Type: NotificationType.Error
       }
     }
-  }, (resp: Array<User>, error: boolean) => {
-    if (error || resp.length < limit) {
-      return
-    }
-    getUsers(offset + limit, limit)
+  }, () => {
+    // TODO
   })
+  onMenuHide()
 }
+
 onMounted(() => {
   commission.getCommissionSetting({
     Message: {
@@ -284,99 +202,9 @@ onMounted(() => {
     // TODO
   })
 
-  coin.getCoins({
-    Message: {
-      Error: {
-        Title: t('MSG_GET_COINS'),
-        Message: t('MSG_GET_COINS_FAIL'),
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, () => {
-    // TODO
-  })
-
-  if (user.Users.Users.length === 0) {
-    getUsers(0, 500)
-  }
-  if (goods.value.length === 0) {
-    getAppGoods(0, 500)
+  if (coin.AppCoins.AppCoins.length === 0) {
+    getCoins(0, 100)
   }
 })
 
-const showing = ref(false)
-const target = ref({
-  UserID: InvalidID
-} as unknown as PurchaseAmountSetting)
-const start = ref('')
-watch(start, () => {
-  target.value.Start = new Date(start.value).getTime() / 1000
-})
-const end = ref('')
-watch(end, () => {
-  target.value.End = new Date(end.value).getTime() / 1000
-})
-
-const onMenuHide = () => {
-  showing.value = false
-}
-
-const onSubmit = () => {
-  if (target.value.Start <= 0) {
-    return
-  }
-
-  if (selectedUser.value.length > 0) {
-    target.value.UserID = selectedUser.value[0].ID
-  }
-
-  purchaseAmount.createUserPurchaseAmountSetting({
-    TargetUserID: target.value.UserID as string,
-    Info: target.value,
-    Message: {
-      Error: {
-        Title: t('MSG_CREATE_USER_PURCHASE_AMOUNT_SETTING'),
-        Message: t('MSG_CREATE_USER_PURCHASE_AMOUNT_SETTING_FAIL'),
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, () => {
-    // TODO
-  })
-  onMenuHide()
-}
-
-const onCancel = () => {
-  onMenuHide()
-}
-
-const onCreatePurchaseAmountSettingClick = () => {
-  showing.value = true
-  target.value = {
-    UserID: InvalidID
-  } as unknown as PurchaseAmountSetting
-  start.value = ''
-}
-
-const getAppGoods = (offset: number, limit: number) => {
-  good.getAppGoods({
-    Offset: offset,
-    Limit: limit,
-    Message: {
-      Error: {
-        Title: 'MSG_GET_APP_GOODS',
-        Message: 'MSG_GET_APP_GOODS_FAIL',
-        Popup: true,
-        Type: NotifyType.Error
-      }
-    }
-  }, (goods: Array<AppGood>, error: boolean) => {
-    if (error || goods.length < limit) {
-      return
-    }
-    getAppGoods(offset + limit, limit)
-  })
-}
 </script>
