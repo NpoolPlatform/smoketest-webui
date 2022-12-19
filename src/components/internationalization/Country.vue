d429af5f54c1bda17040abc3045bc034479f04b3
lidaqiang <dqli161@163.com>
2022-12-19 17:35:03 +0800

    update

--- src/components/internationalization/Country.vue
+++ src/components/internationalization/Country.vue
@@ -2,38 +2,25 @@
   <q-table
     dense
     flat
-    :title='$t("MSG_COUNTRIES")'
+    :title='$t("MSG_APP_COUNTRIES")'
     :rows='countries'
     row-key='ID'
-    :loading='countryLoading'
     :rows-per-page-options='[20]'
   />
 </template>
 
 <script setup lang='ts'>
-import {
-  NotificationType,
-  useLangStore
-} from 'npool-cli-v2'
-import { computed, onMounted, ref } from 'vue'
+import { getCountries } from 'src/api/g11n'
+import { useAdminAppCountryStore } from 'src/teststore/g11n/appcountry'
+import { computed, onMounted } from 'vue'
 
-const lang = useLangStore()
-const countries = computed(() => lang.Countries)
-const countryLoading = ref(true)
+const country = useAdminAppCountryStore()
+const countries = computed(() => country.AppCountries.AppCountries)
 
 onMounted(() => {
-  lang.getCountries({
-    Message: {
-      Error: {
-        Title: 'MSG_GET_COUNTRIES',
-        Message: 'MSG_GET_COUNTRIES_FAIL',
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, () => {
-    countryLoading.value = false
-  })
+  if (countries.value.length === 0) {
+    getCountries(0, 100)
+  }
 })
 
 </script>
--- src/components/internationalization/Language.vue
+++ src/components/internationalization/Language.vue
@@ -2,104 +2,97 @@
   <q-table
     dense
     flat
-    :title='$t("MSG_LANGUAGES")'
+    :title='$t("MSG_APP_LANGUAGES")'
     :rows='langs'
     row-key='ID'
-    :loading='langLoading'
     :rows-per-page-options='[20]'
-    selection='single'
-    v-model:selected='selectedLang'
+    @row-click='(evt, row, index) => onRowClick(row as AppLang)'
   />
-  <q-table
-    dense
-    flat
-    :title='$t("MSG_APP_LANGUAGES")'
-    :rows='appLangs'
-    :loading='appLangLoading'
-    row-key='ID'
-    :rows-per-page-options='[20]'
+  <q-dialog
+    v-model='showing'
+    @hide='onMenuHide'
+    position='right'
   >
-    <template #top-right>
-      <div class='row indent flat'>
-        <div v-if='!language' class='column justify-center'>
-          <span class='warning'>{{ $t('MSG_SELECT_LANGUAGE') }}</span>
-        </div>
-        <q-btn
-          dense
-          flat
-          class='btn flat'
-          :label='$t("MSG_ADD")'
-          @click='onAdd'
-        />
-      </div>
-    </template>
-  </q-table>
+    <q-card class='popup-menu'>
+      <q-card-section>
+        <span>{{ $t('MSG_UPDATE_APP_LANGUAGE') }}</span>
+      </q-card-section>
+      <q-card-section>
+        <div><q-input v-model='target.Lang' :label='$t("MSG_LANG")' /></div>
+        <div><q-input v-model='target.Logo' :label='$t("MSG_LOGO")' /></div>
+        <div><q-input v-model='target.Name' :label='$t("MSG_NAME")' /></div>
+        <div><q-input v-model='target.Short' :label='$t("MSG_SHORT")' /></div>
+      </q-card-section>
+      <q-card-section>
+        <div><q-toggle dense v-model='target.Main' :label='$t("MSG_MAIN_LANGUAGE")' /></div>
+      </q-card-section>
+      <q-item class='row'>
+        <LoadingButton loading :label='$t("MSG_SUBMIT")' @click='onSubmit' />
+        <q-btn class='btn round' :label='$t("MSG_CANCEL")' @click='onCancel' />
+      </q-item>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script setup lang='ts'>
-import { Language, NotificationType, useAdminLangStore, useLangStore, useLocaleStore } from 'npool-cli-v2'
-import { computed, onMounted, ref } from 'vue'
+import { NotifyType } from 'npool-cli-v4'
+import { getAppLangs } from 'src/api/g11n'
+import { useAdminAppLangStore } from 'src/teststore/g11n/applang'
+import { AppLang } from 'src/teststore/g11n/applang/types'
+import { computed, onMounted, ref, defineAsyncComponent } from 'vue'
 
-const lang = useAdminLangStore()
-const appLang = useLangStore()
-const locale = useLocaleStore()
+const LoadingButton = defineAsyncComponent(() => import('src/components/button/LoadingButton.vue'))
 
-const langLoading = ref(true)
-const appLangLoading = ref(true)
+const lang = useAdminAppLangStore()
+const langs = computed(() => lang.AppLangs.AppLangs)
 
-const langs = computed(() => lang.Languages)
-const appLangs = computed(() => locale.Languages)
-const selectedLang = ref([] as Array<Language>)
-const language = computed(() => selectedLang.value.length > 0 ? selectedLang.value[0] : undefined as unknown as Language)
+const target = ref({} as AppLang)
+const showing = ref(false)
+const updating = ref(false)
 
-onMounted(() => {
-  lang.getLangs({
+const onRowClick = (row: AppLang) => {
+  showing.value = true
+  updating.value = true
+  target.value = { ...row }
+}
+
+const onMenuHide = () => {
+  showing.value = false
+  target.value = { Main: false } as AppLang
+}
+
+const onCancel = () => {
+  onMenuHide()
+}
+
+const onSubmit = (done: () => void) => {
+  lang.updateAppLang({
+    ...target.value,
     Message: {
       Error: {
-        Title: 'MSG_GET_LANGS',
-        Message: 'MSG_GET_LANGS_FAIL',
+        Title: 'MSG_UPDATE_APP_LANGUAGE',
+        Message: 'MSG_UPDATE_APP_LANGUAGE_FAIL',
         Popup: true,
-        Type: NotificationType.Error
+        Type: NotifyType.Error
+      },
+      Info: {
+        Title: 'MSG_UPDATE_APP_LANGUAGE',
+        Message: 'MSG_UPDATE_APP_LANGUAGE_SUCCESS',
+        Popup: true,
+        Type: NotifyType.Success
       }
     }
-  }, () => {
-    langLoading.value = false
-  })
-
-  appLang.getLangs({
-    Message: {
-      Error: {
-        Title: 'MSG_GET_LANGS',
-        Message: 'MSG_GET_LANGS_FAIL',
-        Popup: true,
-        Type: NotificationType.Error
-      }
+  }, (error: boolean) => {
+    done()
+    if (error) {
+      return
     }
-  }, () => {
-    appLangLoading.value = false
-  })
-})
-
-const onAdd = () => {
-  if (!language.value) {
-    return
-  }
-
-  lang.createLang({
-    Info: {
-      LangID: language.value.ID
-    },
-    Message: {
-      Error: {
-        Title: 'MSG_GET_LANGS',
-        Message: 'MSG_GET_LANGS_FAIL',
-        Popup: true,
-        Type: NotificationType.Error
-      }
-    }
-  }, () => {
-    // TODO
+    onMenuHide()
   })
 }
+
+onMounted(() => {
+  if (langs.value.length === 0) { getAppLangs(0, 100) }
+})
 
 </script>
