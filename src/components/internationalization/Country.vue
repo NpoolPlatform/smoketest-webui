5926a32438b77e5f19149753d5af76e6905d01ce
lidaqiang <dqli161@163.com>
2022-12-26 15:17:21 +0800

    filter countries

--- src/components/internationalization/Country.vue
+++ src/components/internationalization/Country.vue
@@ -3,19 +3,31 @@
     dense
     flat
     :title='$t("MSG_APP_COUNTRIES")'
-    :rows='countries'
+    :rows='displayCountries'
     row-key='ID'
     :rows-per-page-options='[20]'
-  />
+  >
+    <template #top-right>
+      <q-input
+        dense
+        class='small'
+        v-model='countryName'
+        :label='$t("MSG_COUNTRY")'
+      />
+    </template>
+  </q-table>
 </template>
 
 <script setup lang='ts'>
 import { getCountries } from 'src/api/g11n'
 import { useAdminAppCountryStore } from 'src/teststore/g11n/appcountry'
-import { computed, onMounted } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 
 const country = useAdminAppCountryStore()
 const countries = computed(() => country.AppCountries.AppCountries)
+
+const countryName = ref('')
+const displayCountries = computed(() => countries.value.filter((el) => el.Country?.toLowerCase().includes(countryName.value?.toLowerCase())))
 
 onMounted(() => {
   if (countries.value.length === 0) {
