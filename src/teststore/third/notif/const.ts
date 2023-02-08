5bb3638483c7e4ca79b2a7587f191e61bdfe9d26
lidaqiang <dqli161@163.com>
2023-02-08 17:01:57 +0800

    change prefix

--- src/teststore/third/notif/const.ts
+++ src/teststore/third/notif/const.ts
@@ -1,6 +1,6 @@
 export enum API {
   CREATE_NOTIFTEMPLATE = '/third/v1/create/notiftemplate',
-  GET_NOTIFTEMPLATES = '/notif/v1/get/notiftemplates',
-  UPDATE_NOTIFTEMPLATE = '/notif/v1/update/notiftemplate',
-  DELETE_NOTIFTEMPLATE = '/notif/v1/delete/notiftemplate'
+  GET_NOTIFTEMPLATES = '/third/v1/get/notiftemplates',
+  UPDATE_NOTIFTEMPLATE = '/third/v1/update/notiftemplate',
+  DELETE_NOTIFTEMPLATE = '/third/v1/delete/notiftemplate'
 }
