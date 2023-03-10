dd22a525babfa5b8491d003e075cf77d491f139f
lidaqiang <dqli161@163.com>
2023-03-10 10:54:06 +0800

    update

--- src/components/good/Good.vue
+++ src/components/good/Good.vue
@@ -18,11 +18,6 @@
     :rows-per-page-options='[20]'
     @row-click='(evt, row, index) => onRowClick(row as AppGood)'
   />
-  <q-card>
-    <q-card-section class='bg-primary text-white'>
-      {{ $t('MSG_ADVERTISEMENT_POSITION') }}
-    </q-card-section>
-  </q-card>
   <q-dialog
     v-model='showing'
     @hide='onMenuHide'
