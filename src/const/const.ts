bbdab92e219079fd9d2412f01e47d5dbcaa0fd52
Zhao KK <kikakkz@hotmail.com>
2023-04-03 16:17:54 +0800

    Use new app id for smoketest webui

--- src/const/const.ts
+++ src/const/const.ts
@@ -1,23 +1,4 @@
-let AppID = 'ff2c5d50-be56-413e-aba5-9c7ad888a769'
-
-if (window.location.host.includes('.ohhash.com')) {
-  AppID = 'ab7895dd-c4d1-48bd-be77-c0f255556a36'
-}
-if (window.location.host.includes('.procyon.vip')) {
-  AppID = 'ff2c5d50-be56-413e-aba5-9c7ad888a769'
-}
-if (window.location.host.includes('.lpsecretbase.com')) {
-  AppID = '9141b71a-ff6e-4229-8cf5-cc71459e9803'
-}
-if (window.location.host.includes('.procyon.npool.top')) {
-  AppID = 'ff2c5d50-be56-413e-aba5-9c7ad888a769'
-}
-if (window.location.host.includes('.ohhash.npool.top')) {
-  AppID = 'ab7895dd-c4d1-48bd-be77-c0f255556a36'
-}
-if (window.location.host.includes('.lpsb.npool.top')) {
-  AppID = '9141b71a-ff6e-4229-8cf5-cc71459e9803'
-}
+let AppID = 'a2aab83c-d1f7-11ed-b015-63e181354267'
 
 export {
   AppID
