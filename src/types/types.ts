2d828611347c268576517cb64530beae5f328c6e
Zhao KK <kikakkz@hotmail.com>
2023-04-05 15:21:57 +0800

    Add cond and arg map

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,12 +3,36 @@ export interface Arg {
   Type: string
 }
 
+export enum CondType {
+  PreCondition = 'PreCondition',
+  Cleaner = 'Cleaner'
+}
+
+export enum ArgType {
+  Input = 'Input',
+  Output = 'Output'
+}
+
+export interface ArgMap {
+  ID: string
+  Type: ArgType
+  Src: string
+  Dest: string
+}
+
+export interface Cond {
+  ID: string
+  Index: number
+  Type: CondType
+  ArgMap: Array<ArgMap>
+}
+
 export interface TestCase {
   ID: string
   Domain: string
   Path: string
   Name: string
   Args: Array<Arg>
-  PreConditions: Array<string>
-  Cleaners: Array<string>
+  PreConds: Array<Cond>
+  Cleaners: Array<Cond>
 }
